df7e663da4ca3f81da98aa5cb52baed59130921c
Matthew McKenzie <mojomckenzie@knights.ucf.edu>
2021-07-12 20:05:41 -0400

    deletions

--- client/src/config/firebaseAdmin.js
+++
@@ -1,9 +0,0 @@
-import admin, { credential as _credential } from "firebase-admin";
-
-if(admin.apps.length == 0){
-  admin.initializeApp({
-    credential: _credential.applicationDefault()
-  });
-}
-
-export default admin.auth()
--- client/src/pages/api/getSessions.js
+++
@@ -1,5 +0,0 @@
-
-
-export default function getSessions(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
